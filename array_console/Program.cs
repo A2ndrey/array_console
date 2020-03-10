bbbe4273b3e17a8625a670d0afdfa4ad160aa40b
AnDreW <andrew21ls@yandex.ru>
2020-03-10 21:47:30 +0300

    array in c sharp

--- array_console/Program.cs
+++ array_console/Program.cs
@@ -10,6 +10,108 @@ namespace array_console
     {
         static void Main(string[] args)
         {
+            // test();
+            // multiplyofthree(true);
+            // multiplyofthree(false);
+            //morethanprevious();
+            swapelement();
+
+            Console.ReadKey();
+
+        }
+
+
+        private static void swapelement()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            string[] a = new string[n];
+            string trash = "";
+
+            a = s.Split(' ');
+
+            for (int i = 0; i < n; i = i+2)
+            {
+                if ((i + 1) <= (n - 1))
+                {
+                    trash = a[i + 1];
+                    a[i + 1] = a[i];
+                    a[i] = trash;
+                }
+
+            }
+
+            foreach (string el in a)
+                Console.Write(el +" ");
+
+            //            Sample Input 1:
+            //5
+            //9 4 5 2 3
+            //Sample Output 1:
+            //4 9 2 5 3
+            //Sample Input 2:
+            //4
+            //1 2 3 4
+            //Sample Output 2:
+            //2 1 4 3
+        }
+        private static void morethanprevious()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            string[] array = new string[n];
+            array = s.Split(' ');
+            int previous = Convert.ToInt32(array[0]);
+
+            var count = 0;
+
+            foreach (string a in array)
+            {
+
+                if (Convert.ToInt32(a) > previous)
+                    count++;
+                previous = Convert.ToInt32(a);
+            }
+
+            Console.WriteLine(count);
+
+
+        }
+
+
+        private static void multiplyofthree(bool needindex = false)
+        {
+
+            const int digit = 3;
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int checknumber;
+            string s = Console.ReadLine();
+            string[] array = new string[n];
+            array = s.Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+
+                checknumber = needindex ? i : Convert.ToInt32(array[i]);
+                if (checknumber % digit == 0)
+                    Console.Write(array[i] + " ");
+            }
+
+        }
+
+        public static void test()
+        {
+            Random rnd = new Random();
+
+            int[] test_array = new int[10];
+            for (int i = 0; i < 10; i++)
+                test_array[i] = rnd.Next(0, 10);
+
+            foreach (int a in test_array)
+                Console.WriteLine(a);
+
+            int length = test_array.Length;
+            Console.WriteLine($"Длина массива: {length}");
         }
     }
 }
